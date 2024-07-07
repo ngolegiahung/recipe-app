e97b4a8302d78466b52c7f136c17d44aa97e2f6f
Coroutine <hung.l.g.ngo@gmail.com>
2024-07-07 20:05:52 +0700

    Update to show all info for recipe page.

--- src/test/java/com/springframework/recipe_app/controllers/RecipeControllerTest.java
+++ src/test/java/com/springframework/recipe_app/controllers/RecipeControllerTest.java
@@ -14,8 +14,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 class RecipeControllerTest {
 
@@ -45,7 +44,8 @@ class RecipeControllerTest {
 
         mockMvc.perform(get("/recipe/1"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("recipe/index"));
+                .andExpect(view().name("recipe/index"))
+                .andExpect(model().attributeExists("recipe"));
 
         verify(recipeService, times(1)).findById(anyLong());
         verify(recipeService, never()).getRecipes();
