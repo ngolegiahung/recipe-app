6f7d267e55fd8a7172ee3da558a18657cd86427d
Hung L.G. NGO <hung.l.g.ngo@gmail.com>
2024-07-22 21:16:31 +0700

    Upload image recipe from db to view.

--- src/main/java/com/springframework/recipe_app/service/ImageServiceImpl.java
+++ src/main/java/com/springframework/recipe_app/service/ImageServiceImpl.java
@@ -1,20 +1,37 @@
 package com.springframework.recipe_app.service;
 
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.springframework.recipe_app.domain.Recipe;
+import com.springframework.recipe_app.repositories.RecipeRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-@Slf4j
+import java.io.IOException;
+
 @Service
 public class ImageServiceImpl implements ImageService {
 
-    private static final Logger log = LoggerFactory.getLogger(ImageServiceImpl.class);
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
 
     @Override
     public void saveImageFile(Long recipeId, MultipartFile file) {
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
 
-        log.debug("Received a file");
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+
+            int i = 0;
+            for (byte b : file.getBytes()) {
+                byteObjects[i++] = b;
+            }
+
+            recipe.setImage(byteObjects);
+            recipeRepository.save(recipe);
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
