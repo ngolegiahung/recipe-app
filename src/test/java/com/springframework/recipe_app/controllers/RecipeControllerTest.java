8a978e2bd1520878280e0a12a7e7889a3b4f394d
Hung L.G. NGO <hung.l.g.ngo@gmail.com>
2024-08-06 21:17:08 +0700

    Customizing Error Messages with Message Source and Internationalization with Spring MVC.

--- src/test/java/com/springframework/recipe_app/controllers/RecipeControllerTest.java
+++ src/test/java/com/springframework/recipe_app/controllers/RecipeControllerTest.java
@@ -114,6 +114,7 @@ class RecipeControllerTest {
         mockMvc.perform(post("/recipe")
                         .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                         .param("id", "")
+                        .param("cookTime", "3000")
                 )
                 .andExpect(status().isOk())
                 .andExpect(view().name("recipe/recipeform"));
